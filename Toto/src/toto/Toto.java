58a5b2775266cc16f7f75957f884c42be6f07dc9
andreyyak <andreyyak@LAB24-1.school.npmg.org>
2024-12-04 16:47:59 +0200

    bababoy

--- Toto/src/toto/Toto.java
+++ Toto/src/toto/Toto.java
@@ -14,7 +14,7 @@ public class Toto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code juyuapplication logic here
+        // bababoy
     }
     
 }
