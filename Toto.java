026c61a81e5ba690ca2161505b7237c4c54d57ae
MFNamedAndy <andrey.yav.konov@gmail.com>
2024-12-21 14:22:26 +0200

    Add files via upload

---
+++ Toto.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package toto;
+
+/**
+ *
+ * @author andreyyak
+ */
+public class Toto  {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public class Bilet{ 
+    private Bilet winningNumbers[];
+    
+
+
+   
+    public Bilet[] getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    
+
+
+  
+    public void setWiningNumbers(Bilet[] winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    
+       }
+    
+}
